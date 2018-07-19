06641a4762fd45060393a422e0a684f528e317d6
Your Name <you@example.com>
2018-07-19 21:17:03 +0900

    My Django Girls app, first commit

---
+++ .gitignore.py
@@ -0,0 +1,9 @@
+Python 3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:57:36) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> *.pyc
+*~
+__pycache__
+myvenv
+db.sqlite3
+/static
+.DS_Store
